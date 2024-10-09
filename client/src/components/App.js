75f9a2f3103a2ebb3bec449f2a05fd214d818ae4
Monalisa Onyango <+>
2024-10-09 23:35:15 +0300

    full stack

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -11,10 +11,21 @@ function App() {
   const [messages, setMessages] = useState([]);
   const [search, setSearch] = useState("");
 
+  // useEffect(() => {
+  //   fetch("http://127.0.0.1:5555/messages")
+  //     .then((r) => r.json())
+  //     .then((messages) => setMessages(messages));
+  // }, []);
+
   useEffect(() => {
     fetch("http://127.0.0.1:5555/messages")
-      .then((r) => r.json())
-      .then((messages) => setMessages(messages));
+      .then(r => {
+        if (r.ok) {
+          return r.json()
+        }
+        throw r;
+      })
+      .then((messages) => setMessages(messages))
   }, []);
 
   function handleAddMessage(newMessage) {
